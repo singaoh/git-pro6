4c15e1f18ed253105905cc84e7520f0deced2c5e
Mandy Vlassopoulou <adamantia.vlassopoulou@gmail.com>
2024-05-22 17:48:06 +0300

    HelloWorldApp added bang

--- src/gr/aueb/cf/HelloWorldApp.java
+++ src/gr/aueb/cf/HelloWorldApp.java
@@ -2,7 +2,7 @@ package gr.aueb.cf;
 
 public class HelloWorldApp {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!!");
     }
 
 }
